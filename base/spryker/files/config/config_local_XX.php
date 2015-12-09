4b8354f338ac78da059a09666c38eee23b8728b2
Ehsan Zanjani <ehsanmx@gmail.com>
2015-12-09 18:06:01 +0100

    #3 change SystemConfig/YvesConfig to ApplicationConfig

--- base/spryker/files/config/config_local_XX.php
+++ base/spryker/files/config/config_local_XX.php
@@ -3,53 +3,52 @@
  * !!! This file is maintained by salt. Do not modify this file, as the changes will be overwritten!
  *
  */
-use SprykerFeature\Shared\System\SystemConfig;
-use SprykerFeature\Shared\Yves\YvesConfig;
+use SprykerFeature\Shared\Application\ApplicationConfig;
 
 $environment = '{{ environment }}';
 
 /** Database credentials */
-$config[SystemConfig::ZED_DB_USERNAME] =                   '{{ settings.environments[environment].stores[store].zed.database.username }}';
-$config[SystemConfig::ZED_DB_PASSWORD] =                   '{{ settings.environments[environment].stores[store].zed.database.password }}';
-$config[SystemConfig::ZED_DB_DATABASE] =                   '{{ settings.environments[environment].stores[store].zed.database.database }}';
-$config[SystemConfig::ZED_DB_HOST] =                       '{{ settings.environments[environment].stores[store].zed.database.hostname }}';
-$config[SystemConfig::ZED_DB_PORT] =                       3306;
-$config[SystemConfig::PROPEL]['database']['connections']['default']['dsn'] = 'mysql:host=' . $config[SystemConfig::ZED_DB_HOST]  . ';dbname=' . $config[SystemConfig::ZED_DB_DATABASE];
+$config[ApplicationConfig::ZED_DB_USERNAME] =                   '{{ settings.environments[environment].stores[store].zed.database.username }}';
+$config[ApplicationConfig::ZED_DB_PASSWORD] =                   '{{ settings.environments[environment].stores[store].zed.database.password }}';
+$config[ApplicationConfig::ZED_DB_DATABASE] =                   '{{ settings.environments[environment].stores[store].zed.database.database }}';
+$config[ApplicationConfig::ZED_DB_HOST] =                       '{{ settings.environments[environment].stores[store].zed.database.hostname }}';
+$config[ApplicationConfig::ZED_DB_PORT] =                       3306;
+$config[ApplicationConfig::PROPEL]['database']['connections']['default']['dsn'] = 'mysql:host=' . $config[ApplicationConfig::ZED_DB_HOST]  . ';dbname=' . $config[ApplicationConfig::ZED_DB_DATABASE];
 
 
 /** Public URL's and domains */
 $yvesHost =                                                '{{ settings.environments[environment].stores[store].yves.hostnames[0] }}';
-$config[SystemConfig::HOST_YVES] =                         'http://' . $yvesHost;
-$config[SystemConfig::HOST_SSL_YVES] =                     'https://' . $yvesHost;
-$config[YvesConfig::YVES_SSL_ENABLED] =                    true;
-$config[YvesConfig::YVES_COMPLETE_SSL_ENABLED] =           true;
+$config[ApplicationConfig::HOST_YVES] =                         'http://' . $yvesHost;
+$config[ApplicationConfig::HOST_SSL_YVES] =                     'https://' . $yvesHost;
+$config[ApplicationConfig::YVES_SSL_ENABLED] =                    true;
+$config[ApplicationConfig::YVES_COMPLETE_SSL_ENABLED] =           true;
 
-$config[SystemConfig::HOST_STATIC_ASSETS] =
-    $config[SystemConfig::HOST_STATIC_MEDIA] =             '{{ settings.environments[environment].static.hostname }}';
+$config[ApplicationConfig::HOST_STATIC_ASSETS] =
+    $config[ApplicationConfig::HOST_STATIC_MEDIA] =             '{{ settings.environments[environment].static.hostname }}';
 
-$config[SystemConfig::HOST_SSL_STATIC_ASSETS] =
-    $config[SystemConfig::HOST_SSL_STATIC_MEDIA] =         '{{ settings.environments[environment].static.hostname }}';
+$config[ApplicationConfig::HOST_SSL_STATIC_ASSETS] =
+    $config[ApplicationConfig::HOST_SSL_STATIC_MEDIA] =         '{{ settings.environments[environment].static.hostname }}';
 
 $zedHost =                                                 '{{ settings.environments[environment].stores[store].zed.hostname }}';
-$config[SystemConfig::HOST_ZED_GUI] =                      'http://' . $zedHost;
-$config[SystemConfig::HOST_ZED_API] =                      $zedHost;
-$config[SystemConfig::HOST_SSL_ZED_GUI] =
-    $config[SystemConfig::HOST_SSL_ZED_API] =              'https://' . $zedHost;
-$config[SystemConfig::ZED_SSL_ENABLED] =                   true;
-$config[SystemConfig::ZED_API_SSL_ENABLED] =               true;
+$config[ApplicationConfig::HOST_ZED_GUI] =                      'http://' . $zedHost;
+$config[ApplicationConfig::HOST_ZED_API] =                      $zedHost;
+$config[ApplicationConfig::HOST_SSL_ZED_GUI] =
+    $config[ApplicationConfig::HOST_SSL_ZED_API] =              'https://' . $zedHost;
+$config[ApplicationConfig::ZED_SSL_ENABLED] =                   true;
+$config[ApplicationConfig::ZED_API_SSL_ENABLED] =               true;
 
-$config[YvesConfig::YVES_SESSION_COOKIE_DOMAIN] =          $yvesHost;
+$config[ApplicationConfig::YVES_SESSION_COOKIE_DOMAIN] =          $yvesHost;
 
 /** Elasticsearch */
-$config[SystemConfig::ELASTICA_PARAMETER__HOST] =          '{{ settings.hosts.elasticsearch_data|first }}';
-$config[SystemConfig::ELASTICA_PARAMETER__TRANSPORT] =     'http';
-$config[SystemConfig::ELASTICA_PARAMETER__PORT] =          '{{ settings.environments[environment].elasticsearch.http_port }}';
-$config[SystemConfig::ELASTICA_PARAMETER__INDEX_NAME] =    '{{ store|lower }}_{{ environment }}_catalog';
-$config[SystemConfig::ELASTICA_PARAMETER__DOCUMENT_TYPE] = 'page';
+$config[ApplicationConfig::ELASTICA_PARAMETER__HOST] =          '{{ settings.hosts.elasticsearch_data|first }}';
+$config[ApplicationConfig::ELASTICA_PARAMETER__TRANSPORT] =     'http';
+$config[ApplicationConfig::ELASTICA_PARAMETER__PORT] =          '{{ settings.environments[environment].elasticsearch.http_port }}';
+$config[ApplicationConfig::ELASTICA_PARAMETER__INDEX_NAME] =    '{{ store|lower }}_{{ environment }}_catalog';
+$config[ApplicationConfig::ELASTICA_PARAMETER__DOCUMENT_TYPE] = 'page';
 
 /** RabbitMQ */
-$config[SystemConfig::ZED_RABBITMQ_HOST] =                 '{{ settings.host.queue }}';
-$config[SystemConfig::ZED_RABBITMQ_PORT] =                 '5672';
-$config[SystemConfig::ZED_RABBITMQ_USERNAME] =             '{{ settings.environments[environment].stores[store].rabbitmq.username }}';
-$config[SystemConfig::ZED_RABBITMQ_PASSWORD] =             '{{ settings.environments[environment].stores[store].rabbitmq.password }}';
-$config[SystemConfig::ZED_RABBITMQ_VHOST] =                '{{ settings.environments[environment].stores[store].rabbitmq.vhost }}';
+$config[ApplicationConfig::ZED_RABBITMQ_HOST] =                 '{{ settings.host.queue }}';
+$config[ApplicationConfig::ZED_RABBITMQ_PORT] =                 '5672';
+$config[ApplicationConfig::ZED_RABBITMQ_USERNAME] =             '{{ settings.environments[environment].stores[store].rabbitmq.username }}';
+$config[ApplicationConfig::ZED_RABBITMQ_PASSWORD] =             '{{ settings.environments[environment].stores[store].rabbitmq.password }}';
+$config[ApplicationConfig::ZED_RABBITMQ_VHOST] =                '{{ settings.environments[environment].stores[store].rabbitmq.vhost }}';
