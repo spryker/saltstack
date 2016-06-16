2579df861553e5fe2a428dde13e7a1199f4fdd33
marius-rizac <marius.rizac@spryker.com>
2016-06-16 12:48:08 +0000

    added new alias

--- dev/development/files/home/vagrant/oh-my-zsh/custom/plugins/spryker/spryker.plugin.zsh
+++ dev/development/files/home/vagrant/oh-my-zsh/custom/plugins/spryker/spryker.plugin.zsh
@@ -2,6 +2,8 @@ alias composer='php -d xdebug.remote_enable=0 composer.phar'
 alias ci='php -d xdebug.remote_enable=0 composer.phar install'
 alias cu='php -d xdebug.remote_enable=0 composer.phar update'
 
+alias debug='XDEBUG_CONFIG="remote_host=10.10.0.1" PHP_IDE_CONFIG="serverName=zed.de.spryker.dev"'
+
 codecept () {
     APPLICATION_ENV=development APPLICATION_STORE=DE /data/shop/development/current/vendor/bin/codecept run $*
 }
