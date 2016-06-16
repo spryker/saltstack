9aa81c816447ec5325925c8c2707d616d6f1ab3c
Marius Rizac <marius.rizac@spryker.com>
2016-06-16 14:24:49 +0200

    fixed console aliases

--- dev/development/files/home/vagrant/oh-my-zsh/custom/plugins/spryker/spryker.plugin.zsh
+++ dev/development/files/home/vagrant/oh-my-zsh/custom/plugins/spryker/spryker.plugin.zsh
@@ -3,19 +3,13 @@ alias ci='php -d xdebug.remote_enable=0 composer.phar install'
 alias cu='php -d xdebug.remote_enable=0 composer.phar update'
 
 codecept () {
-    pushd /data/shop/development/current
-    APPLICATION_ENV=development APPLICATION_STORE=DE vendor/bin/codecept run $*
-    popd
+    APPLICATION_ENV=development APPLICATION_STORE=DE /data/shop/development/current/vendor/bin/codecept run $*
 }
 
 debug-console () {
-    pushd /data/shop/development/current
-    XDEBUG_CONFIG="remote_host=10.10.0.1" PHP_IDE_CONFIG="serverName=zed.spryker.dev" vendor/bin/console $*
-    popd
+    XDEBUG_CONFIG="remote_host=10.10.0.1" PHP_IDE_CONFIG="serverName=zed.spryker.dev" /data/shop/development/current/vendor/bin/console $*
 }
 
 console () {
-    pushd /data/shop/development/current
-    vendor/bin/console $*
-    popd
+    /data/shop/development/current/vendor/bin/console $*
 }
