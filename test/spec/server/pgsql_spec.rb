67d63b764093ed8f89f01d73665c3866a59ee9bc
Marek Obuchowicz <marek@korekontrol.eu>
2017-02-15 13:06:25 +0100

    Fix pgsql test

--- test/spec/server/pgsql_spec.rb
+++ test/spec/server/pgsql_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'PostgreSQL' do
-  env['PGPASSWORD'] = 'mate20mg'
+  ENV['PGPASSWORD'] = 'mate20mg'
 
   describe command("psql --user development --host 127.0.0.1 DE_development_zed -c \"SELECT * FROM pg_extension WHERE extname='citext'\"") do
     its(:stdout) { should include('1 row') }
