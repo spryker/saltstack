8ca472443e1bf90d57ceaa930c9855d9f18de818
Marek Obuchowicz <marek@korekontrol.eu>
2017-03-14 13:02:38 +0100

    Fix tests

--- test/spec/server/mysql_spec.rb
+++ test/spec/server/mysql_spec.rb
@@ -7,7 +7,7 @@ describe 'MySQL' do
     its(:stdout) { should include('Ver 5.7') }
   end
 
-  describe command("myqsl #{credentials} -e 'show databases'") do
+  describe command("mysql #{credentials} -e 'show databases'") do
     its(:stdout) { should include('DE_development_zed') }
     its(:stdout) { should include('DE_devtest_zed') }
     its(:stdout) { should include('US_development_zed') }
