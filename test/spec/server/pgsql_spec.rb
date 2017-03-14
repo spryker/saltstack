7683a537eaf52e47f9c5ec70383880bf23cce9dc
Marek Obuchowicz <marek@korekontrol.eu>
2017-03-14 11:21:08 +0100

    Add tests for database states

--- test/spec/server/pgsql_spec.rb
+++ test/spec/server/pgsql_spec.rb
@@ -6,4 +6,11 @@ describe 'PostgreSQL' do
   describe command("psql --user development --host 127.0.0.1 DE_development_zed -c \"SELECT * FROM pg_extension WHERE extname='citext'\"") do
     its(:stdout) { should include('1 row') }
   end
+
+  describe command("psql --user development --host 127.0.0.1 DE_development_zed -c \"SELECT datname FROM pg_database WHERE datistemplate = false\"") do
+    its(:stdout) { should include('DE_development_zed') }
+    its(:stdout) { should include('DE_devtest_zed') }
+    its(:stdout) { should include('US_development_zed') }
+    its(:stdout) { should include('US_devtest_zed') }
+  end
 end
