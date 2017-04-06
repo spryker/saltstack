9c944758b27435c0dd1525791ec1526ebd18b1ee
Marek Obuchowicz <marek@korekontrol.eu>
2017-04-06 12:30:50 +0200

    Fix tests

--- test/spec/server/php_spec.rb
+++ test/spec/server/php_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'php' do
   let(:path) { '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
-  PHP_VERSION='7.1'
+  PHP_VERSION = '7.1'
 
-  describe service("php${PHP_VERSION}-fpm") do
+  describe service("php#{PHP_VERSION}-fpm") do
     it { should be_enabled }
     it { should be_running }
   end
@@ -15,7 +15,7 @@ describe 'php' do
 
   # By default xdebug is disabled
   describe command('php -v') do
-    its(:stdout) { should include("PHP ${PHP_VERSION}") }
+    its(:stdout) { should include("PHP #{PHP_VERSION}") }
     its(:stdout) { should include('with Zend OPcache') }
     its(:stdout) { should_not include('with Xdebug') }
   end
@@ -26,7 +26,7 @@ describe 'php' do
     its(:stderr) { should_not include('ERROR') }
   end
 
-  describe command("php-fpm${PHP_VERSION} -i") do
+  describe command("php-fpm#{PHP_VERSION} -i") do
     its(:stdout) { should include('opcache.enable => On => On') }
     its(:stderr) { should_not include('NOTICE') }
     its(:stderr) { should_not include('WARNING') }
@@ -34,11 +34,11 @@ describe 'php' do
   end
 
   # Commands from README.md for enabling / disabling xdebug
-  describe command("phpenmod -v ${PHP_VERSION} -s cli -m xdebug; phpenmod -v ${PHP_VERSION} -s fpm -m xdebug && service php${PHP_VERSION}-fpm restart && php -v") do
+  describe command("phpenmod -v #{PHP_VERSION} -s cli -m xdebug; phpenmod -v #{PHP_VERSION} -s fpm -m xdebug && service php#{PHP_VERSION}-fpm restart && php -v") do
     its(:stdout) { should include('with Xdebug') }
   end
 
-  describe command("phpdismod -v ${PHP_VERSION} -s cli -m xdebug; phpdismod -v ${PHP_VERSION} -s fpm -m xdebug; service php${PHP_VERSION}-fpm restart; php -v") do
+  describe command("phpdismod -v #{PHP_VERSION} -s cli -m xdebug; phpdismod -v #{PHP_VERSION} -s fpm -m xdebug; service php#{PHP_VERSION}-fpm restart; php -v") do
     its(:stdout) { should_not include('with Xdebug') }
   end
 
