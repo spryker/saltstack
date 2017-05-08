52a160faa6cc2694b031b46d74038a5f8622575b
Marek Obuchowicz <marek@korekontrol.eu>
2017-05-08 15:01:39 +0200

    Update constant names in config files

--- base/spryker/files/config/config_local.php
+++ base/spryker/files/config/config_local.php
@@ -2,21 +2,27 @@
 /**
  * !!! This file is maintained by salt. Do not modify this file, as the changes will be overwritten!
  */
-use SprykerFeature\Shared\Application\ApplicationConstants;
+use Spryker\Shared\Session\SessionConstants;
+use Spryker\Shared\Storage\StorageConstants;
+use Spryker\Shared\Setup\SetupConstants;
 
-/** Session storage */
-$config[ApplicationConstants::ZED_SESSION_SAVE_HANDLER]
-    = $config[ApplicationConstants::YVES_SESSION_SAVE_HANDLER]
-    = 'redis';
-$config[ApplicationConstants::YVES_STORAGE_SESSION_REDIS_PROTOCOL] = 'tcp';
-$config[ApplicationConstants::YVES_STORAGE_SESSION_REDIS_HOST] = '{{ settings.host.redis }}';
-$config[ApplicationConstants::YVES_STORAGE_SESSION_REDIS_PORT] = '{{ settings.environments[environment].redis.port }}';
+/** Session and KV storage */
+$config[StorageConstants::STORAGE_REDIS_PROTOCOL] = 'tcp';
+$config[StorageConstants::STORAGE_REDIS_HOST] = '{{ settings.host.redis }}';
+$config[StorageConstants::STORAGE_REDIS_PORT] = '{{ settings.environments[environment].redis.port }}';
+$config[StorageConstants::STORAGE_REDIS_PASSWORD] = '';
+$config[StorageConstants::STORAGE_REDIS_DATABASE] = 0;
 
+$config[SessionConstants::YVES_SESSION_REDIS_PROTOCOL] = 'tcp';
+$config[SessionConstants::YVES_SESSION_REDIS_HOST] = '{{ settings.host.redis }}';
+$config[SessionConstants::YVES_SESSION_REDIS_PORT] = '{{ settings.environments[environment].redis.port }}';
+$config[SessionConstants::YVES_SESSION_REDIS_DATABASE] = 1;
 
-$config[ApplicationConstants::ZED_STORAGE_SESSION_REDIS_PROTOCOL] = $config[ApplicationConstants::YVES_STORAGE_SESSION_REDIS_PROTOCOL];
-$config[ApplicationConstants::ZED_STORAGE_SESSION_REDIS_HOST] = $config[ApplicationConstants::YVES_STORAGE_SESSION_REDIS_HOST];
-$config[ApplicationConstants::ZED_STORAGE_SESSION_REDIS_PORT] = $config[ApplicationConstants::YVES_STORAGE_SESSION_REDIS_PORT];
+$config[SessionConstants::ZED_SESSION_REDIS_PROTOCOL] = 'tcp';
+$config[SessionConstants::ZED_SESSION_REDIS_HOST] = '{{ settings.host.redis }}';
+$config[SessionConstants::ZED_SESSION_REDIS_PORT] = '{{ settings.environments[environment].redis.port }}';
+$config[SessionConstants::ZED_SESSION_REDIS_DATABASE] = 2;
 
 /** Jenkins */
-$config[ApplicationConstants::JENKINS_BASE_URL] = 'http://{{ settings.hosts.job|first }}:{{ settings.environments[environment].jenkins.port }}/';
-$config[ApplicationConstants::JENKINS_DIRECTORY] = '/data/shop/{{ environment }}/shared/data/common/jenkins';
+$config[SetupConstants::JENKINS_BASE_URL] = 'http://{{ settings.hosts.job|first }}:{{ settings.environments[environment].jenkins.port }}/';
+$config[SetupConstants::JENKINS_DIRECTORY] = '/data/shop/{{ environment }}/shared/data/common/jenkins';
--- base/spryker/files/config/config_local_XX.php
+++ base/spryker/files/config/config_local_XX.php
@@ -3,25 +3,25 @@
  * !!! This file is maintained by salt. Do not modify this file, as the changes will be overwritten!
  *
  */
-use SprykerFeature\Shared\Application\ApplicationConstants;
+use Spryker\Shared\Propel\ApplicationConstants;
+use Spryker\Shared\Propel\PropelConstants;
+use Spryker\Shared\Propel\SessionConstants;
 
 $environment = '{{ environment }}';
 
 /** Database credentials */
-$config[ApplicationConstants::ZED_DB_USERNAME] =                   '{{ settings.environments[environment].stores[store].zed.database.username }}';
-$config[ApplicationConstants::ZED_DB_PASSWORD] =                   '{{ settings.environments[environment].stores[store].zed.database.password }}';
-$config[ApplicationConstants::ZED_DB_DATABASE] =                   '{{ settings.environments[environment].stores[store].zed.database.database }}';
-$config[ApplicationConstants::ZED_DB_HOST] =                       '{{ settings.environments[environment].stores[store].zed.database.hostname }}';
-$config[ApplicationConstants::ZED_DB_PORT] =                       3306;
-$config[ApplicationConstants::PROPEL]['database']['connections']['default']['dsn'] = 'mysql:host=' . $config[ApplicationConstants::ZED_DB_HOST]  . ';dbname=' . $config[ApplicationConstants::ZED_DB_DATABASE];
+$config[PropelConstants::ZED_DB_USERNAME] =                   '{{ settings.environments[environment].stores[store].zed.database.username }}';
+$config[PropelConstants::ZED_DB_PASSWORD] =                   '{{ settings.environments[environment].stores[store].zed.database.password }}';
+$config[PropelConstants::ZED_DB_DATABASE] =                   '{{ settings.environments[environment].stores[store].zed.database.database }}';
+$config[PropelConstants::ZED_DB_HOST] =                       '{{ settings.environments[environment].stores[store].zed.database.hostname }}';
+$config[PropelConstants::ZED_DB_PORT] =                       5432;
+//$config[ApplicationConstants::PROPEL]['database']['connections']['default']['dsn'] = 'mysql:host=' . $config[ApplicationConstants::ZED_DB_HOST]  . ';dbname=' . $config[ApplicationConstants::ZED_DB_DATABASE];
 
 
 /** Public URL's and domains */
 $yvesHost =                                                '{{ settings.environments[environment].stores[store].yves.hostnames[0] }}';
 $config[ApplicationConstants::HOST_YVES] =                         'http://' . $yvesHost;
 $config[ApplicationConstants::HOST_SSL_YVES] =                     'https://' . $yvesHost;
-$config[ApplicationConstants::YVES_SSL_ENABLED] =                    true;
-$config[ApplicationConstants::YVES_COMPLETE_SSL_ENABLED] =           true;
 
 $config[ApplicationConstants::HOST_STATIC_ASSETS] =
     $config[ApplicationConstants::HOST_STATIC_MEDIA] =             '{{ settings.environments[environment].static.hostname }}';
@@ -34,10 +34,8 @@ $config[ApplicationConstants::HOST_ZED_GUI] =                      'http://' . $
 $config[ApplicationConstants::HOST_ZED_API] =                      $zedHost;
 $config[ApplicationConstants::HOST_SSL_ZED_GUI] =
     $config[ApplicationConstants::HOST_SSL_ZED_API] =              'https://' . $zedHost;
-$config[ApplicationConstants::ZED_SSL_ENABLED] =                   true;
-$config[ApplicationConstants::ZED_API_SSL_ENABLED] =               true;
 
-$config[ApplicationConstants::YVES_SESSION_COOKIE_DOMAIN] =          $yvesHost;
+$config[SessionConstants::YVES_SESSION_COOKIE_DOMAIN] =          $yvesHost;
 
 /** Elasticsearch */
 $config[ApplicationConstants::ELASTICA_PARAMETER__HOST] =          '{{ settings.hosts.elasticsearch_data|first }}';
@@ -47,8 +45,8 @@ $config[ApplicationConstants::ELASTICA_PARAMETER__INDEX_NAME] =    '{{ store|low
 $config[ApplicationConstants::ELASTICA_PARAMETER__DOCUMENT_TYPE] = 'page';
 
 /** RabbitMQ */
-$config[ApplicationConstants::ZED_RABBITMQ_HOST] =                 '{{ settings.host.queue }}';
-$config[ApplicationConstants::ZED_RABBITMQ_PORT] =                 '5672';
-$config[ApplicationConstants::ZED_RABBITMQ_USERNAME] =             '{{ settings.environments[environment].stores[store].rabbitmq.username }}';
-$config[ApplicationConstants::ZED_RABBITMQ_PASSWORD] =             '{{ settings.environments[environment].stores[store].rabbitmq.password }}';
-$config[ApplicationConstants::ZED_RABBITMQ_VHOST] =                '{{ settings.environments[environment].stores[store].rabbitmq.vhost }}';
+// $config[ApplicationConstants::ZED_RABBITMQ_HOST] =                 '{{ settings.host.queue }}';
+// $config[ApplicationConstants::ZED_RABBITMQ_PORT] =                 '5672';
+// $config[ApplicationConstants::ZED_RABBITMQ_USERNAME] =             '{{ settings.environments[environment].stores[store].rabbitmq.username }}';
+// $config[ApplicationConstants::ZED_RABBITMQ_PASSWORD] =             '{{ settings.environments[environment].stores[store].rabbitmq.password }}';
+// $config[ApplicationConstants::ZED_RABBITMQ_VHOST] =                '{{ settings.environments[environment].stores[store].rabbitmq.vhost }}';
